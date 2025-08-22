ea94f9367a3da682d676854462778eb9b47a8144
Ben Taussig <btaussig97@gmail.com>
2025-08-22 10:58:20 -0400

    add ball animation

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
+const N_ANIMATION_FRAMES = 16
+const BIN_SIZE = 360/N_ANIMATION_FRAMES
 
 func _on_body_entered(body: Node) -> void:
 	if body.get_parent() is Paddle:
 		body.get_parent().apply_collision_force(self)
+
+func _physics_process(delta: float) -> void:
+	var rotation_degrees_normalized = rotation_degrees
+	while rotation_degrees_normalized < 360:
+		rotation_degrees_normalized += 360
+	rotation_degrees_normalized = int(rotation_degrees_normalized) % 360
+	var frame = int((N_ANIMATION_FRAMES - 1) -  (rotation_degrees_normalized/BIN_SIZE))
+	$AnimatedSprite2D.frame = frame
