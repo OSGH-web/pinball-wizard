f6c16593fbc111158668e5cfe19d03a1139a4f33
jaketaussig <jaketaussig@gmail.com>
2025-08-24 10:59:29 -0700

    Changes all instances of "Paddle" to "Flipper"

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -4,11 +4,13 @@ const N_ANIMATION_FRAMES = 16
 const BIN_SIZE = 360/N_ANIMATION_FRAMES
 
 func _on_body_entered(body: Node) -> void:
-	if body.get_parent() is Paddle:
+	if body.get_parent() is Flipper:
 		body.get_parent().apply_collision_force(self)
 
+# Just for selecting a frame
 func _physics_process(delta: float) -> void:
 	var rotation_degrees_normalized = rotation_degrees
+	# Mapping rotation within 360
 	while rotation_degrees_normalized < 360:
 		rotation_degrees_normalized += 360
 	rotation_degrees_normalized = int(rotation_degrees_normalized) % 360
