edbe78926a769af3aa915f3e20d227c007010d3e
Ben Taussig <btaussig97@gmail.com>
2025-08-25 16:27:16 -0400

    add scoring to rollover button

--- scripts/rollover_button.gd
+++ scripts/rollover_button.gd
@@ -6,10 +6,13 @@ enum RolloverButtonState { UNSET, SET }
 
 var state = RolloverButtonState.UNSET
 
+const score_value = 500
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if state == RolloverButtonState.UNSET:
+		if body is Ball:
+			body.add_to_score.emit(score_value)
 		set_rolloverbutton()
 
 
