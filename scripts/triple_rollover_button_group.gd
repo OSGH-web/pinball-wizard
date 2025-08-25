fff226087c7527e8849464ec9303e56b91b60c96
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-08-25 16:49:39 -0400

    Create triple rollover button group (#14)
    * add triple rollover button group
    
    * add scoring to rollover button
    
    * remove unncessary signal connection for first Ball
    
    * emit signal when rollover button is scored; listen in triple rollover group
    
    * Add multiplier to main.gd
    
    * modifications to main.tscn
    
    * remove debug/testing setup
    
    ---------
    
    Co-authored-by: Ben Taussig <btaussig97@gmail.com>

---
+++ scripts/triple_rollover_button_group.gd
@@ -0,0 +1,71 @@
+extends Node2D
+class_name TripleRolloverButtonGroup
+
+
+
+var buttons = []
+
+signal increment_multiplier
+
+func _ready():
+	buttons = [$RolloverButton, $RolloverButton2, $RolloverButton3]
+
+	for button: RolloverButton in buttons:
+		button.connect("button_is_set", check_if_all_buttons_are_set)
+
+func check_if_all_buttons_are_set():
+	var all_set := true
+	for button: RolloverButton in buttons:
+		if button.state != RolloverButton.RolloverButtonState.SET:
+			all_set = false
+			break
+
+	if all_set:
+		increment_multiplier.emit()
+		reset_all_buttons()
+
+func reset_all_buttons():
+	for button: RolloverButton in buttons:
+		button.reset_rolloverbutton()
+
+func shift_right():
+	var button_states = []
+	for button in buttons:
+		button_states.append(button.state)
+
+	button_states.push_front(button_states[2])
+	button_states.remove_at(3)
+
+	for i in range(3):
+		var new_state = button_states[i]
+		var button = buttons[i]
+
+		if button.state != new_state:
+			if new_state == RolloverButton.RolloverButtonState.SET:
+				button.set_rolloverbutton()
+			else:
+				button.reset_rolloverbutton()
+
+func shift_left():
+	var button_states = []
+	for button in buttons:
+		button_states.append(button.state)
+
+	button_states.push_back(button_states[0])
+	button_states.remove_at(0)
+
+	for i in range(3):
+		var new_state = button_states[i]
+		var button = buttons[i]
+
+		if button.state != new_state:
+			if new_state == RolloverButton.RolloverButtonState.SET:
+				button.set_rolloverbutton()
+			else:
+				button.reset_rolloverbutton()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_right"):
+		shift_right()
+	elif event.is_action_pressed("ui_left"):
+		shift_left()
