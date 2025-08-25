e8ac92262a0da2d5f9e8998a1a6ae0480e591183
Ben Taussig <btaussig97@gmail.com>
2025-08-25 16:41:28 -0400

    emit signal when rollover button is scored; listen in triple rollover group

--- scripts/rollover_button.gd
+++ scripts/rollover_button.gd
@@ -8,12 +8,16 @@ var state = RolloverButtonState.UNSET
 
 const score_value = 500
 
+signal button_is_set
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if state == RolloverButtonState.UNSET:
+		set_rolloverbutton()
+
 		if body is Ball:
 			body.add_to_score.emit(score_value)
-		set_rolloverbutton()
+			button_is_set.emit()
 
 
 func reset_rolloverbutton():
