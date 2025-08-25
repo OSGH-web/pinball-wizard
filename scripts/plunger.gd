3e5e7e02c9149c0f9d774e154877a3d57ebaae6c
Ben Taussig <btaussig97@gmail.com>
2025-08-25 08:27:34 -0400

    buffer plunger inputs

--- scripts/plunger.gd
+++ scripts/plunger.gd
@@ -4,21 +4,44 @@ class_name Plunger
 const PLUNGER_STRENGTH = 30
 
 # Called when the node enters the scene tree for the first time.
-enum PlungerState { RISING, FALLING, IDLE }  # Flipper motion phases
-enum PlungerInput { NONE, PRESS, RELEASE }   
+enum PlungerState { RISING, FALLING, IDLE }  # Plunger motion phases
+enum PlungerInput { NONE, PRESS, RELEASE }
 
 var state = PlungerState.IDLE
+var input := PlungerInput.NONE
+
 
 func apply_collision_force(ball: RigidBody2D):
-	if state != PlungerState.FALLING and state != PlungerState.IDLE:
+	if state == PlungerState.RISING:
 		var force = Vector2(0, -1) * PLUNGER_STRENGTH
 		ball.apply_central_impulse(force)
-		
-	
+
+
 func _input(event):
 	if event.is_action_pressed("lower_plunger"):
-		$AnimationPlayer.play_section("Lower")
-		state = PlungerState.FALLING
-	elif $PlungerSprites.frame == 3 and event.is_action_released("lower_plunger"):
-		$AnimationPlayer.play_backwards("Lower")
-		state = PlungerState.RISING
+		input = PlungerInput.PRESS
+	elif event.is_action_released("lower_plunger"):
+		input = PlungerInput.RELEASE
+
+
+func _ready():
+	$AnimationPlayer.connect("animation_finished", func(anim_name: StringName):
+		if anim_name == "Lower" && $PlungerSprites.frame == 0:
+			state = PlungerState.IDLE
+		)
+
+
+func _physics_process(delta: float):
+	match state:
+		PlungerState.IDLE:
+			if input == PlungerInput.PRESS:
+				$AnimationPlayer.play("Lower")
+				state = PlungerState.FALLING
+				input = PlungerInput.NONE
+		PlungerState.FALLING:
+			if $PlungerSprites.frame == 3 and input == PlungerInput.RELEASE:
+				$AnimationPlayer.play_backwards("Lower")
+				state = PlungerState.RISING
+				input = PlungerInput.NONE
+		PlungerState.RISING:
+			pass
