52013939f847ad14d644b321f2a300c7bd43161a
Ben Taussig <btaussig97@gmail.com>
2025-08-26 20:42:19 -0400

    force trip wire collision depending on type

--- scripts/trip_wire.gd
+++ scripts/trip_wire.gd
@@ -4,6 +4,18 @@ enum TripWireType { STEP_UP, STEP_DOWN }
 
 @export var type: TripWireType = TripWireType.STEP_UP
 
+func _ready():
+	if type == TripWireType.STEP_UP:
+		set_collision_layer_value(1, true)
+		set_collision_mask_value(1, true)
+		set_collision_layer_value(2, false)
+		set_collision_mask_value(2, false)
+	elif type == TripWireType.STEP_DOWN:
+		set_collision_layer_value(1, false)
+		set_collision_mask_value(1, false)
+		set_collision_layer_value(2, true)
+		set_collision_mask_value(2, true)
+
 func _on_body_entered(body: Node2D) -> void:
 	if not body is Ball:
 		return
