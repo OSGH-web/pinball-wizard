53f94da5bcccff6cd487ad1c176ea82d41170a61
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-08-28 11:51:39 -0400

    Add Audio Toggle (#40)
    * add audio toggle
    
    * don't do anything if the player has clicked off of the board
    
    * reconnect background music finished signal
    
    * reconnect shake layer script (merge conflicts)
    
    ---------
    
    Co-authored-by: Ben Taussig <btaussig97@gmail.com>

---
+++ scripts/audio_toggle.gd
@@ -0,0 +1,37 @@
+extends Button
+
+enum AudioToggleState { NORMAL, MUTED }
+
+var state: AudioToggleState = AudioToggleState.NORMAL
+
+
+func _on_mouse_entered() -> void:
+	if state == AudioToggleState.NORMAL:
+		$AudioSheet.frame = 1 # show mute icon
+	else:
+		$AudioSheet.frame = 0 # show normal icon
+
+
+func _on_mouse_exited() -> void:
+	if state == AudioToggleState.NORMAL:
+		$AudioSheet.frame = 0  # show normal icon
+	else:
+		$AudioSheet.frame = 1  # show mute icon
+
+
+func _on_button_down() -> void:
+	if state == AudioToggleState.NORMAL:
+		state = AudioToggleState.MUTED
+		$AudioSheet.frame = 1  # show mute icon
+		mute()
+	else:
+		state = AudioToggleState.NORMAL
+		$AudioSheet.frame = 0  # show normal icon
+		unmute()
+
+func mute():
+	AudioServer.set_bus_mute(0, true)
+
+
+func unmute():
+	AudioServer.set_bus_mute(0, false)
