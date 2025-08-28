a238beef5581972a0891258d4948ce723485bf55
jaketaussig <jaketaussig@gmail.com>
2025-08-28 17:32:32 -0400

    Game loop implemented

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,7 +6,10 @@ var timer: Timer
 # The amount of time the timer will start with in milliseconds
 var countdown_time :=  180
 var game_started := false
+var game_round = 0
+var score_goal = 0
 var ball_scene = preload("res://scenes/ball.tscn")
+var damage_number_scene = preload("res://scenes/damage_number.tscn")
 var score := 0:
 	set(new_score):
 		if new_score < 0:
@@ -19,8 +22,7 @@ var multiplier = 1:
 			multiplier = 1
 		else:
 			multiplier = new_multiplier
-
-var damage_number_scene = preload("res://scenes/damage_number.tscn")
+			
 
 func _ready():
 	%UI/TimerLabel.text = format_time(countdown_time)
@@ -30,7 +32,22 @@ func _ready():
 	%Spinner.connect("spin_points", _modify_score)
 	%Spinner2.connect("spin_points", _modify_score)
 	%SpeedBoost.connect("modify_score", _modify_score)
+	_calculate_and_set_score_goal()
+	%BallCapture.connect("update_timer_and_score_goal", _update_timer_and_score_goal)
 	_create_new_ball()
+	
+	
+func _process(_delta: float) -> void:
+	if game_started:
+		var remaining = int(ceil(%UI/Timer.time_left))
+		%UI/TimerLabel.text = format_time(remaining)
+	# This will be called every frame when the score goal is reached. Could make more efficient.
+	if score >= score_goal:
+		%Gate.open_gate()
+		if %BallCapture.state == BallCapture.BallCaptureState.INACTIVE:
+			%BallCapture.state = BallCapture.BallCaptureState.ACTIVE
+	elif score < score_goal and %CloseGateTimer.is_stopped():
+		%CloseGateTimer.start()
 
 
 func _create_new_ball():
@@ -44,11 +61,23 @@ func _create_new_ball():
 func _on_game_started_signal():
 	_start_countdown(countdown_time)
 	
-	
+
+# time is in seconds	
 func _add_time(time):
 	%UI/Timer.set_wait_time(%UI/Timer.time_left + time)
 	%UI/Timer.start()
 	
+
+# This should only be called when the ball is locked after reaching a score goal
+func _calculate_and_set_score_goal():
+	score_goal = ((2**game_round) * 16000)
+	%ScoreGoal.text = "Goal: \n" + str(score_goal)
+	game_round += 1
+	
+func _update_timer_and_score_goal():
+	_add_time(60)
+	_calculate_and_set_score_goal()
+
 	
 func _modify_score(score_value: int, _new_ball: bool = false):
 	# Might need to change - right now the multiplier will also multipy the death penalty. 
@@ -71,12 +100,6 @@ func _modify_score(score_value: int, _new_ball: bool = false):
 func _modify_multiplier(mult_value: int):
 	multiplier += mult_value
 	%UI/HBoxContainer/Multiplier.text = "x%d" % multiplier
-	
-	
-func _process(_delta: float) -> void:
-	if game_started:
-		var remaining = int(ceil(%UI/Timer.time_left))
-		%UI/TimerLabel.text = format_time(remaining)
 
 
 func _start_countdown(duration: int):
@@ -86,12 +109,6 @@ func _start_countdown(duration: int):
 	game_started = true
 	
 	
-func _on_timer_timeout() -> void:
-	# Player loses the game
-	# print to catch potential game crash error. 
-	print("END GAME")
-
-
 func format_time(seconds: int) -> String:
 	var minutes = seconds / 60
 	var secs = seconds % 60
@@ -101,3 +118,14 @@ func format_time(seconds: int) -> String:
 func _on_shake_layer_child_entered_tree(node: Node) -> void:
 	if node is TripleRolloverButtonGroup:
 		node.connect("modify_multiplier", _modify_multiplier)
+		
+		
+func _on_timer_timeout() -> void:
+	# Player loses the game
+	# print to catch potential game crash error. 
+	print("END GAME")
+
+
+func _on_close_gate_timer_timeout() -> void:
+	%Gate.close_gate()
+	%BallCapture.state = BallCapture.BallCaptureState.INACTIVE
